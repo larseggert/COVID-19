e00b1363db8cb9123333aa74c278bba5999b4777
Lars Eggert <lars@eggert.org>
2020-03-30 17:24:54 +0300

    Add R plot script

---
+++ plot.r
@@ -0,0 +1,40 @@
+#! /usr/bin/env Rscript
+
+if (!require("pacman"))
+	install.packages("pacman", repos = "https://cloud.r-project.org/")
+pacman::p_load(tidyverse, cowplot, lubridate)
+
+data = read_csv("csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+
+data = rename(data, Country = "Country/Region")
+
+data = select(data, Country, ends_with("/20"))
+
+data = filter(data, Country %in% commandArgs(trailingOnly=TRUE))
+
+data = pivot_longer(data, cols=ends_with("/20"), names_to="Date", values_to="Count")
+
+data = mutate(data, Date=mdy(Date))
+
+data = filter(data, Count > 1)
+
+data = mutate(data, Diff=Count - lag(Count), DiffFract=Diff/Count)
+
+data
+
+f <- function(start, x) 10^(x-start)
+
+p1 = ggplot(data, aes(x=Date, y=Count, group=Country, color=Country)) +
+	scale_y_log10(limits=c(1, NA)) +
+	geom_point() +
+	geom_smooth() +
+	theme_minimal_grid() + theme(legend.position="bottom")
+
+p2 = ggplot(data, aes(x=Date, y=DiffFract, group=Country, color=Country)) +
+	scale_y_continuous(limits=c(-1, NA)) +
+	coord_cartesian(ylim=c(0, NA)) +
+	geom_point() +
+	geom_smooth() +
+	theme_minimal_grid() + theme(legend.position="bottom")
+
+plot_grid(p1, p2)
