cf69ed480b88e7c6596895798c31927aac61e516
Lars Eggert <lars@eggert.org>
2020-04-23 16:12:40 +0300

    Tweaks

--- plot.r
+++ plot.r
@@ -2,11 +2,13 @@
 
 if (!require("pacman"))
 	install.packages("pacman", repos = "https://cloud.r-project.org/")
-pacman::p_load(tidyverse, cowplot, lubridate)
+pacman::p_load(tidyverse, cowplot, lubridate, scales)
 
 data = read_csv("csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
 
-data = rename(data, Country = "Country/Region")
+data = rename(data, Country = "Country/Region", State = "Province/State")
+
+data = filter(data, is.na(State))
 
 data = select(data, Country, ends_with("/20"))
 
@@ -25,14 +27,14 @@ print(data, n=1000)
 f <- function(start, x) 10^(x-start)
 
 p_cnt = ggplot(data, aes(x=Date, y=Count, group=Country, color=Country)) +
-	scale_y_log10(limits=c(1, NA)) +
+	scale_y_log10(limits=c(1, NA), labels=comma_format(accuracy=1)) +
 	geom_point() +
 	geom_smooth() +
 	labs(y="Cases") +
 	theme_minimal_grid() + theme(legend.position="bottom")
 
 p_diff = ggplot(data, aes(x=Date, y=Diff, group=Country, color=Country)) +
-	scale_y_continuous(limits=c(-100, NA)) +
+	scale_y_continuous(limits=c(-100, NA), labels=comma_format(accuracy=1)) +
 	coord_cartesian(ylim=c(0, NA)) +
 	geom_point() +
 	geom_smooth() +
@@ -40,7 +42,7 @@ p_diff = ggplot(data, aes(x=Date, y=Diff, group=Country, color=Country)) +
 	theme_minimal_grid() + theme(legend.position="bottom")
 
 p_difffract = ggplot(data, aes(x=Date, y=DiffFract, group=Country, color=Country)) +
-	scale_y_continuous(limits=c(-1, NA)) +
+	scale_y_continuous(limits=c(-1, NA), labels=comma_format(accuracy=1)) +
 	coord_cartesian(ylim=c(0, NA)) +
 	geom_point() +
 	geom_smooth() +
